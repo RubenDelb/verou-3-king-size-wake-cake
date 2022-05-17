6fb8379aa1fcf2e32790cabdc03a0572cb67e5db
Dericioso <dery.lantigua@gmail.com>
2022-05-17 18:38:35 +0200

    Create different components and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
 import './App.css';
-import Square from './Components/Square/Square';
+import { Header, Footer, Home, Design, Display, Circle, Square } from './Components';
+import { Routes, Route } from 'react-router-dom'
+
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
+
+      <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/" element={<Design />} />
+      <Route exact path="/" element={<Display />} />
+      </Routes>
+      
       <Square />
+      <Circle />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/index.jsx
@@ -0,0 +1,17 @@
+import Header from './Header';
+import Footer from './Footer';
+import Home from './Home';
+import Design from './Design/Design';
+import Display from './Display/Display';
+import Circle from './Circle/Circle';
+import Square from './Square/Square';
+
+export {
+    Header,
+    Footer,
+    Home,
+    Design,
+    Display,
+    Circle,
+    Square,
+}
