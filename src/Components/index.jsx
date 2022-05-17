ec63860d178de1fafa208a933005bdba497dbeaf
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-17 18:41:37 +0200

    Update the imports

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,10 +1,10 @@
 import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
-import Design from './Design/Design';
+import Design from './CakeDesign/CakeDesign';
 import Display from './Display/Display';
-import Circle from './Circle/Circle';
-import Square from './Square/Square';
+import Circle from './CakeDesign/Circle/Circle';
+import Square from './CakeDesign/Square/Square';
 
 export {
     Header,
