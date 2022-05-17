a600e9a4b573566d77957e60fd2a69d5d7f704c8
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-17 19:06:13 +0200

    Update imports

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,6 +1,6 @@
-import Header from './Header';
-import Footer from './Footer';
-import Home from './Home';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Home from './Home/Home';
 import CakeDesign from './CakeDesign/CakeDesign';
 import Display from './Display/Display';
 
