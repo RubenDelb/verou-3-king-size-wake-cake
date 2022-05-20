ec7f51f6a7187fb4dd7877f7ee6961189e031c11
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-20 11:26:15 +0200

    Add framer-motion + add shapes

--- src/Components/CakeDesign/CakeDesign.jsx
+++ src/Components/CakeDesign/CakeDesign.jsx
@@ -1,15 +1,43 @@
 import Square from "./Square/Square";
 import Circle from "./Circle/Circle";
+import { motion } from "framer-motion";
 
 import './CakeDesign.scss';
+import Heart from "./Heart/Heart";
 
 
 const CakeDesign = () => {
     return (
-        <div>
-            <Square />
-            <Circle />
-        </div>
+        <>
+            <motion.div 
+                animate={{ x:150, y:50 }}
+                transition={{ duration:3 }}
+                whileInView={{ opacity:[0,1] }}
+            >
+                <Heart />
+            </motion.div>
+            <motion.div 
+                animate={{ x:450, y:50 }}
+                transition={{ duration:3 }}
+                whileInView={{ opacity:[0,1] }}
+            >
+                <Circle />
+            </motion.div>
+            <motion.div 
+                animate={{ x:750, y:50 }}
+                transition={{ duration:3 }}
+                whileInView={{ opacity:[0,1] }}
+            >
+                <Square />
+            </motion.div>
+            <motion.h1 
+                animate={{ x:100 }}
+                transition={{ duration:3 }}
+                whileInView={{ opacity:[0,1] }}
+            >
+                Hello Dery
+            </motion.h1>
+        </>
     )
 }
 
