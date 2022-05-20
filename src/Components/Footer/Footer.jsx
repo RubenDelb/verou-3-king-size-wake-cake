b428c2f48cd72f960f83e2af1b40a115da4f5e9c
Dericioso <dery.lantigua@gmail.com>
2022-05-20 14:12:15 +0200

    Gave functionally to the header and footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,13 @@ import './Footer.scss';
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <>
+        <div className=''>
+            <p>Authors: Dery & Ruben © 2022 All Right Reserve</p>
+            <a href="mailto:dery.lantigua@gmail.com">Email Dery</a> <br />
+            <a href="mailto:ruben.delbrassine@gmail.com">Email Ruben</a>
+        </div>
+        </>
     )
 }
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,18 @@
 import './Header.scss';
+import { Link } from 'react-router-dom';
+import Button from '../Button/Button';
+
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <>
+        <h1>Cakery</h1>
+        <div className='nav'>
+            <Link to={"/"}><Button btnName={'Home'} /></Link>
+            <Link to="/cakedesign"><Button btnName={'Cake Design'} /></Link>
+            <Link to="/display-cake"><Button btnName={'My Designs'} /></Link>
+        </div>
+        </>
     )
 }
 
