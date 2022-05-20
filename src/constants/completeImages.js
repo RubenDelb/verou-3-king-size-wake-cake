251a18a47c204349ab2e1d65eb187e0ae16dc166
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-20 17:11:03 +0200

    Make objects with more info about each cake

--- src/constants/completeImages.js
+++ src/constants/completeImages.js
@@ -7,10 +7,10 @@ import StrawberryPie from '../assets/CompletePies/StrawberryPie.png';
 
 
 export default [
-    CarrotPie,
-    ChocolatePie,
-    LemonPie,
-    MintPie,
-    RedVelvetPie,
-    StrawberryPie
+    { src: CarrotPie, name: "Carrot Cake", description: "Our sweet and delicious carrot pie is a delicious dessert for both spring and fall, or anytime you want a unique pie for your family."},
+    { src: ChocolatePie, name: "Chocolate Cake", description: "A divine creamy chocolate cake that makes you float away in your deepest desires."} ,
+    { src: LemonPie, name: "Lemon Pie", description: "Delicious Lemon pie."},
+    { src: MintPie, name: "Mint Cake", description: "Minty mint Cake :-)"},
+    { src: RedVelvetPie, name: "Red Velvet cake", description: "A dense and soft cake with a moist and velvety crumb."},
+    { src: StrawberryPie, name: "Strawberry cake", description: "When you're eating this strawberry cake, it instantly makes you dream about hopping through a strawberry field."}
 ]
