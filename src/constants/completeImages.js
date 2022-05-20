a2b1666aba7b90030708f922eec71f203fecc04e
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-20 14:57:11 +0200

    Rename images and export them in one file

---
+++ src/constants/completeImages.js
@@ -0,0 +1,16 @@
+import CarrotPie from '../assets/CompletePies/CarrotPie.png';
+import ChocolatePie from '../assets/CompletePies/ChocolatePie.png';
+import LemonPie from '../assets/CompletePies/LemonPie.png';
+import MintPie from '../assets/CompletePies/MintPie.png';
+import RedVelvetPie from '../assets/CompletePies/RedVelvetPie.png';
+import StrawberryPie from '../assets/CompletePies/StrawberryPie.png';
+
+
+export default [
+    CarrotPie,
+    ChocolatePie,
+    LemonPie,
+    MintPie,
+    RedVelvetPie,
+    StrawberryPie
+]
