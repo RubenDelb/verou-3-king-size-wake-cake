e382c8b1a6db240adce9df6a0b0f0a50b8ef8410
Dericioso <dery.lantigua@gmail.com>
2022-05-24 17:12:16 +0200

    Modify the header with its stylying

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import './Header.scss';
+import { Link } from 'react-router-dom';
+import Button from '../Button/Button';
 
 const Header = () => {
     return (
+        <>
         <div className='header'>
-            <div className='p-text'>Custom Pizza</div>
+            <h1 className='title'>Custom Pizza</h1>
+            <div>
+                <Link to={"/"}><Button btnName={'Home'} /></Link>
+                <Link to="/customize"><Button btnName={'Customize Pizza'} /></Link>
+                {/* <Link to="/AddRecipe"><Button btnName={'Get Inspire'} /></Link> */}
+            </div>
         </div>
+        </>
     )
 }
 
