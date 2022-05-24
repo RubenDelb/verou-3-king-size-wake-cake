51c46715107dd9ba535ffad45c99c3b6bd38c8ec
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-24 15:38:02 +0200

    Change name because its already used for usestate

--- src/constants/ingredientsImages.js
+++ src/constants/ingredientsImages.js
@@ -4,12 +4,14 @@ import Mushroom from '../assets/ingredients/Mushroom.png';
 import Olive from '../assets/ingredients/Olive.png';
 import Pineapple from '../assets/ingredients/Pineapple.png';
 import Tomato from '../assets/ingredients/Tomato.png';
+import Base from '../assets/ingredients/PizzaBase.png';
 
 export default [
     { src: Cheese, name: "cheese", zIndex: "z1"},
-    { src: Basil, name: "basil", zIndex: "z4"},
+    { src: Basil, name: "basil", zIndex: "z5"},
     { src: Mushroom, name: "mushroom", zIndex: "z4"},
     { src: Olive, name: "olive", zIndex: "z4"},
     { src: Pineapple, name: "pineapple", zIndex: "z3"},
     { src: Tomato, name: "tomato", zIndex: "z4"},
+    { src: Base, name: "pizzabase", zIndex: "z1"},
 ]
