85e6064415e7c13d3f899c5b41d50ed9b6bef127
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-24 13:50:07 +0200

    Map through ingredients to dynamically display + styling

--- src/Components/Customize/Customize.jsx
+++ src/Components/Customize/Customize.jsx
@@ -1,8 +1,33 @@
 import './Customize.scss';
+import { ingredients } from '../../constants';
+import { motion } from 'framer-motion';
 
 const Customize = () => {
   return (
-    <div>Customize</div>
+    <div className='customize'>
+        <div className='image-container'>
+            <div className='inner-image-container'>
+                {ingredients.map((ingredient) => {
+                    return (
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{
+                                y: ingredients[ingredient.name] ? 100 : -100,
+                                opacity: ingredients[ingredient.name] ? 1 : 0,
+                            }}
+                            transition={{ duration: 1 }}
+                            className={`ingredients ${ingredient.zIndex} ${ingredient.name}`}
+                        >
+                            <img src={ingredient.src} alt={ingredient.name} height="100%" width="100%" />
+                        </motion.div>
+                    )
+                })}
+            </div>
+        </div>
+        <div className='checkboxes-container'>
+            checkboxes
+        </div>
+    </div>
   )
 }
 
