a13a91317543bfee5a7e6949b8a3a76ebcb04c27
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-24 15:34:46 +0200

    Save checked ingredients to localstorage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
-import { Header, Footer, Home, CakeDesign, Display, Carousel } from './components';
+import { Header, Footer, Home } from './components';
 import { Routes, Route } from 'react-router-dom';
 import Customize from './components/Customize/Customize';
+import Checkout from './components/Checkout/Checkout';
 
 const App = () => {
+  const [ingredients, setIngredients] = useState({
+    basil: false,
+    cheese: false,
+    mushroom: false,
+    olive: false,
+    pineapple: false,
+    tomato: false,
+  });
+
+  useEffect(() => {
+    const data = localStorage.getItem("ingredients");
+    if (data) {
+      setIngredients(JSON.parse(data));
+    }
+  }, []);
+
   return (
     <div className="app">
       <Header />
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/customize" element={<Customize />} />
-        <Route path="/checkout" element={<h1>Checkout</h1>} />
+        <Route path="/customize" element={<Customize ingredients={ ingredients } setIngredients={ setIngredients }/>} />
+        <Route path="/checkout" element={<Checkout ingredients={ingredients} />} />
       </Routes>
 
       <Footer />
