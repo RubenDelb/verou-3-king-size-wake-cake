ca7ebcc0ebeec4493320e8ecddcfa0ba8a96b42f
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-24 11:14:11 +0200

    Undo import of deleted images

--- src/constants/completeImages.js
+++ src/constants/completeImages.js
@@ -1,16 +1,16 @@
-import CarrotPie from '../assets/CompletePies/CarrotPie.png';
-import ChocolatePie from '../assets/CompletePies/ChocolatePie.png';
-import LemonPie from '../assets/CompletePies/LemonPie.png';
-import MintPie from '../assets/CompletePies/MintPie.png';
-import RedVelvetPie from '../assets/CompletePies/RedVelvetPie.png';
-import StrawberryPie from '../assets/CompletePies/StrawberryPie.png';
+// import CarrotPie from '../assets/CompletePies/CarrotPie.png';
+// import ChocolatePie from '../assets/CompletePies/ChocolatePie.png';
+// import LemonPie from '../assets/CompletePies/LemonPie.png';
+// import MintPie from '../assets/CompletePies/MintPie.png';
+// import RedVelvetPie from '../assets/CompletePies/RedVelvetPie.png';
+// import StrawberryPie from '../assets/CompletePies/StrawberryPie.png';
 
 
-export default [
-    { src: CarrotPie, name: "Carrot Cake", description: "Our sweet and delicious carrot pie is a delicious dessert for both spring and fall, or anytime you want a unique pie for your family."},
-    { src: ChocolatePie, name: "Chocolate Cake", description: "A divine creamy chocolate cake that makes you float away in your deepest desires."} ,
-    { src: LemonPie, name: "Lemon Pie", description: "Delicious Lemon pie."},
-    { src: MintPie, name: "Mint Cake", description: "Minty mint Cake :-)"},
-    { src: RedVelvetPie, name: "Red Velvet cake", description: "A dense and soft cake with a moist and velvety crumb."},
-    { src: StrawberryPie, name: "Strawberry cake", description: "When you're eating this strawberry cake, it instantly makes you dream about hopping through a strawberry field."}
-]
+// export default [
+//     { src: CarrotPie, name: "Carrot Cake", description: "Our sweet and delicious carrot pie is a delicious dessert for both spring and fall, or anytime you want a unique pie for your family."},
+//     { src: ChocolatePie, name: "Chocolate Cake", description: "A divine creamy chocolate cake that makes you float away in your deepest desires."} ,
+//     { src: LemonPie, name: "Lemon Pie", description: "Delicious Lemon pie."},
+//     { src: MintPie, name: "Mint Cake", description: "Minty mint Cake :-)"},
+//     { src: RedVelvetPie, name: "Red Velvet cake", description: "A dense and soft cake with a moist and velvety crumb."},
+//     { src: StrawberryPie, name: "Strawberry cake", description: "When you're eating this strawberry cake, it instantly makes you dream about hopping through a strawberry field."}
+// ]
