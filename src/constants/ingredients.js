561bb8fbdcc16c4e74dd648340706aa863805a14
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-24 13:51:31 +0200

    Import ingredients to export as an array of objects

---
+++ src/constants/ingredients.js
@@ -0,0 +1,15 @@
+import Cheese from '../assets/ingredients/BaseCheese.png';
+import Basil from '../assets/ingredients/Basil.png';
+import Mushroom from '../assets/ingredients/Mushroom.png';
+import Olive from '../assets/ingredients/Olive.png';
+import Pineapple from '../assets/ingredients/Pineapple.png';
+import Tomato from '../assets/ingredients/Tomato.png';
+
+export default [
+    { src: Cheese, name: "cheese", zIndex: "z1"},
+    { src: Basil, name: "basil", zIndex: "z4"},
+    { src: Mushroom, name: "mushroom", zIndex: "z4"},
+    { src: Olive, name: "olive", zIndex: "z4"},
+    { src: Pineapple, name: "pineapple", zIndex: "z3"},
+    { src: Tomato, name: "tomato", zIndex: "z4"},
+]
