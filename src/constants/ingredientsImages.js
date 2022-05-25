2c8dbb95be164b3aec8205add7221278ee0b3b5f
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-25 16:21:59 +0200

    Add and import new images

--- src/constants/ingredientsImages.js
+++ src/constants/ingredientsImages.js
@@ -5,13 +5,19 @@ import Olive from '../assets/ingredients/Olive.png';
 import Pineapple from '../assets/ingredients/Pineapple.png';
 import Tomato from '../assets/ingredients/Tomato.png';
 import Base from '../assets/ingredients/PizzaBase.png';
+import Broccoli from '../assets/ingredients/broccolis.png';
+import Carrot from '../assets/ingredients/carrots.png';
+import Eggplant from '../assets/ingredients/eggplants.png';
 
 export default [
     { src: Cheese, name: "cheese", zIndex: "z1"},
     { src: Basil, name: "basil", zIndex: "z5"},
     { src: Mushroom, name: "mushroom", zIndex: "z4"},
-    { src: Olive, name: "olive", zIndex: "z4"},
+    { src: Olive, name: "olive", zIndex: "z3"},
     { src: Pineapple, name: "pineapple", zIndex: "z3"},
     { src: Tomato, name: "tomato", zIndex: "z4"},
+    { src: Broccoli, name: "broccoli", zIndex: "z4"},
+    { src: Carrot, name: "carrot", zIndex: "z4"},
+    { src: Eggplant, name: "eggplant", zIndex: "z4"},
     { src: Base, name: "pizzabase", zIndex: "z1"},
 ]
