607255205fa069253f6e62025b01fbe16a19eabf
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-27 15:51:13 +0200

    Export Custom-Components in one file

--- src/Components/Customisation/index.js
+++ src/Components/Customisation/index.js
@@ -1,13 +1,19 @@
-import CustomBase from './CustomBase';
-import CustomSauce from './CustomSauce';
-import CustomTopping from './CustomTopping';
-import CustomCheese from './CustomCheese';
-import CustomRemarks from './CustomRemarks';
+// import CustomBase from './CustomBase/CustomBase';
+// import CustomSauce from './CustomSauce/CustomSauce';
+// import CustomTopping from './CustomTopping/CustomTopping';
+// import CustomCheese from './CustomCheese/CustomCheese';
+// import CustomRemarks from './CustomRemarks/CustomRemarks';
 
-export {
-    CustomBase,
-    CustomSauce,
-    CustomTopping,
-    CustomCheese,
-    CustomRemarks,
-}
+export { default as CustomBase } from './CustomBase/CustomBase'
+export { default as CustomSauce } from './CustomSauce/CustomSauce'
+export { default as CustomTopping } from './CustomTopping/CustomTopping'
+export { default as CustomCheese } from './CustomCheese/CustomCheese'
+export { default as CustomRemarks } from './CustomRemarks/CustomRemarks'
+
+// export {
+//     CustomBase,
+//     CustomSauce,
+//     CustomTopping,
+//     CustomCheese,
+//     CustomRemarks,
+// }
