47d6dcbb2187a9e4d84daa98df11a4000810ba98
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-27 15:52:21 +0200

    Create a remarks-component for users remarks

--- src/Components/Customisation/CustomRemarks/CustomRemarks.jsx
+++ src/Components/Customisation/CustomRemarks/CustomRemarks.jsx
@@ -1,9 +1,16 @@
-import 'CustomRemarks.scss';
+import { useNavigate } from 'react-router-dom';
+import AppWrap from '../../AppWrap';
+import Button from '../../Button/Button';
+import './CustomRemarks.scss';
 
 const CustomRemarks = () => {
+  let navigate = useNavigate();
+
   return (
-    <div>CustomRemarks</div>
+    <div>CustomRemarks
+      <Button btnName='Checkout!' onClick={() => navigate('/checkout')}/>
+    </div>
   )
 }
 
-export default CustomRemarks
+export default AppWrap(CustomRemarks, 'remarks')
