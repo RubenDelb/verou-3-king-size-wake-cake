c4e7fc5a998ffcde68db59ba4c8ec8485c675768
Dericioso <dery.lantigua@gmail.com>
2022-05-27 10:34:33 +0200

    Modife the route of the images

--- src/constants/completeImages.js
+++ src/constants/completeImages.js
@@ -1,16 +1,13 @@
-// import CarrotPie from '../assets/CompletePies/CarrotPie.png';
-// import ChocolatePie from '../assets/CompletePies/ChocolatePie.png';
-// import LemonPie from '../assets/CompletePies/LemonPie.png';
-// import MintPie from '../assets/CompletePies/MintPie.png';
-// import RedVelvetPie from '../assets/CompletePies/RedVelvetPie.png';
-// import StrawberryPie from '../assets/CompletePies/StrawberryPie.png';
+import MeatLover from '../assets/ingredients2/Carousel/meatlover.png';
+import Mushroom from '../assets/ingredients2/Carousel/mushroom.png';
+import Peperoni from '../assets/ingredients2/Carousel/peperoni.png';
+import Veggie from '../assets/ingredients2/Carousel/veggie.png';
 
 
-// export default [
-//     { src: CarrotPie, name: "Carrot Cake", description: "Our sweet and delicious carrot pie is a delicious dessert for both spring and fall, or anytime you want a unique pie for your family."},
-//     { src: ChocolatePie, name: "Chocolate Cake", description: "A divine creamy chocolate cake that makes you float away in your deepest desires."} ,
-//     { src: LemonPie, name: "Lemon Pie", description: "Delicious Lemon pie."},
-//     { src: MintPie, name: "Mint Cake", description: "Minty mint Cake :-)"},
-//     { src: RedVelvetPie, name: "Red Velvet cake", description: "A dense and soft cake with a moist and velvety crumb."},
-//     { src: StrawberryPie, name: "Strawberry cake", description: "When you're eating this strawberry cake, it instantly makes you dream about hopping through a strawberry field."}
-// ]
+
+export default [
+    { src: MeatLover, name: "Meat Lover", description: "The best for the best. Our meat comes from the best corners in the world."},
+    { src: Mushroom, name: "Mushroom", description: "For those who enjoy the delicatessen of a good mushroom in their pizza."} ,
+    { src: Peperoni, name: "Peperoni", description: "Delicious Peperoni Pizza."},
+    { src: Veggie, name: "Veggie", description: "Veggie in the house :-)"},
+]
