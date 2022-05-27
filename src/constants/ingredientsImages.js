e506cd7959b9d2b5132d8246e971416649271e99
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-27 14:04:26 +0200

    Restructure images into separate files

--- src/constants/ingredientsImages.js
+++ src/constants/ingredientsImages.js
@@ -1,16 +1,13 @@
-import Cheese from '../assets/ingredients/BaseCheese.png';
-import Basil from '../assets/ingredients/Basil.png';
-import Mushroom from '../assets/ingredients/Mushroom.png';
-import Olive from '../assets/ingredients/Olive.png';
-import Pineapple from '../assets/ingredients/Pineapple.png';
-import Tomato from '../assets/ingredients/Tomato.png';
-import Base from '../assets/ingredients/PizzaBase.png';
-import Broccoli from '../assets/ingredients/broccolis.png';
-import Carrot from '../assets/ingredients/carrots.png';
-import Eggplant from '../assets/ingredients/eggplants.png';
+import Basil from '../assets/Topping/Basil.png';
+import Mushroom from '../assets/Topping/Mushroom.png';
+import Olive from '../assets/Topping/Olive.png';
+import Pineapple from '../assets/Topping/Pineapple.png';
+import Tomato from '../assets/Topping/Tomato.png';
+import Broccoli from '../assets/Topping/broccolis.png';
+import Carrot from '../assets/Topping/carrots.png';
+import Eggplant from '../assets/Topping/eggplants.png';
 
 export default [
-    { src: Cheese, name: "cheese", zIndex: "z1"},
     { src: Basil, name: "basil", zIndex: "z5"},
     { src: Mushroom, name: "mushroom", zIndex: "z4"},
     { src: Olive, name: "olive", zIndex: "z3"},
@@ -19,5 +16,4 @@ export default [
     { src: Broccoli, name: "broccoli", zIndex: "z4"},
     { src: Carrot, name: "carrot", zIndex: "z4"},
     { src: Eggplant, name: "eggplant", zIndex: "z4"},
-    { src: Base, name: "pizzabase", zIndex: "z1"},
 ]
