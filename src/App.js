f7ccf5b1e32ce0dca2e80b31293384d8c4c83579
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-27 15:49:18 +0200

    Move the ingredients-checklist to component

--- src/App.js
+++ src/App.js
@@ -5,22 +5,8 @@ import { Routes, Route } from 'react-router-dom';
 import { CustomBase, CustomCheese, CustomRemarks, CustomSauce, CustomTopping } from './Components/Customisation';
 import Checkout from './Components/Checkout/Checkout'
 
-const App = () => {
-  const [ingredients, setIngredients] = useState({
-    basil: false,
-    cheese: false,
-    mushroom: false,
-    olive: false,
-    pineapple: false,
-    tomato: false,
-  });
 
-  useEffect(() => {
-    const data = localStorage.getItem("ingredients");
-    if (data) {
-      setIngredients(JSON.parse(data));
-    }
-  }, []);
+const App = () => {
 
   return (
     <div className="app">
@@ -28,12 +14,12 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/custombase" element={<CustomBase ingredients={ ingredients } setIngredients={ setIngredients }/>} />
-        <Route path="/customsauce" element={<CustomSauce ingredients={ ingredients } setIngredients={ setIngredients }/>} />
-        <Route path="/customtopping" element={<CustomTopping ingredients={ ingredients } setIngredients={ setIngredients }/>} />
-        <Route path="/customcheese" element={<CustomCheese ingredients={ ingredients } setIngredients={ setIngredients }/>} />
-        <Route path="/customremarks" element={<CustomRemarks ingredients={ ingredients } setIngredients={ setIngredients }/>} />
-        <Route path="/checkout" element={<Checkout ingredients={ingredients} />} />
+        <Route path="/custombase" element={<CustomBase />} />
+        <Route path="/customsauce" element={<CustomSauce />} />
+        <Route path="/customtopping" element={<CustomTopping />} />
+        <Route path="/customcheese" element={<CustomCheese/>} />
+        <Route path="/customremarks" element={<CustomRemarks/>} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
 
       <Footer />
