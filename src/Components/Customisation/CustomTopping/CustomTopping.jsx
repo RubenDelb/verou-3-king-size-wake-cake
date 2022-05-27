814687761c0f6a6e4471c069372844c84cea7d8d
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-27 15:53:21 +0200

    Add the ingredients-object and save in localstorage

--- src/Components/Customisation/CustomTopping/CustomTopping.jsx
+++ src/Components/Customisation/CustomTopping/CustomTopping.jsx
@@ -3,12 +3,33 @@ import { ingredientsImages } from '../../../constants';
 import { motion } from 'framer-motion';
 import { useNavigate } from "react-router-dom";
 import Button from '../../Button/Button';
+import AppWrap from '../../AppWrap';
+import { useEffect, useState } from 'react';
 
-const Customize = ( { ingredients, setIngredients } ) => {
-    let navigate = useNavigate();
+const CustomTopping = () => {
+    const [ingredients, setIngredients] = useState({
+        basil: false,
+        cheese: false,
+        mushroom: false,
+        olive: false,
+        pineapple: false,
+        tomato: false,
+    });
+
+    console.log(ingredients);
+
+    useEffect(() => {
+        const data = localStorage.getItem("ingredients");
+        if (data) {
+        setIngredients(JSON.parse(data));
+        }
+    }, []);
+
+
+    console.log(ingredients);
 
     const onChange = (event, name) => {
-        // console.log(localStorage);
+        console.log(localStorage);
         let newIngredients = JSON.parse(JSON.stringify(ingredients));
         newIngredients[name] = event;
         setIngredients(newIngredients);
@@ -22,28 +43,24 @@ const Customize = ( { ingredients, setIngredients } ) => {
                     {ingredientsImages.map((ingredient) => {
                         return (
                             <>
-                                { ingredient.name === "pizzabase" ? (
-                                    <motion.div transition={{ duration: 1 }} className="base">
-                                        <img src={ingredient.src} alt="Pizza Base" height="100%" width="100%" />
-                                    </motion.div> ) : (
-                                    <motion.div
-                                        initial={{ opacity: 0 }}
-                                        animate={{
-                                            y: ingredients[ingredient.name] ? 100 : -100,
-                                            opacity: ingredients[ingredient.name] ? 1 : 0,
-                                        }}
-                                        transition={{ duration: 1 }}
-                                        className={`ingredients ${ingredient.zIndex} ${ingredient.name}`}
-                                        key={ingredient.name}
-                                    >
-                                        <img src={ingredient.src} alt={ingredient.name} height="100%" width="100%" />
-                                    </motion.div>)}
+                                <motion.div
+                                    initial={{ opacity: 0 }}
+                                    animate={{
+                                        y: ingredients[ingredient.name] ? 0 : -200,
+                                        opacity: ingredients[ingredient.name] ? 1 : 0,
+                                    }}
+                                    transition={{ duration: 1 }}
+                                    className={`ingredients ${ingredient.zIndex} ${ingredient.name}`}
+                                    key={ingredient.name}
+                                >
+                                    <img src={ingredient.src} alt={ingredient.name} height="100%" width="100%" />
+                                </motion.div>
                             </>
                         )
                     })}
                 </div>
             </div>
-            
+
             <div className='checkboxes-container'>
                 {ingredientsImages.map((ingredient) => {
                     return (
@@ -64,10 +81,9 @@ const Customize = ( { ingredients, setIngredients } ) => {
                         </>
                     )
                 })}
-                <Button btnName='Checkout!' onClick={() => navigate('/checkout')}/>
             </div>
         </div>
     )
 }
 
-export default Customize
+export default AppWrap(CustomTopping, 'topping')
