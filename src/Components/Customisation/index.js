a67f04dd5de28db95c1427cc38a19f7003bb2cde
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-27 11:02:34 +0200

    Export all components in one file

---
+++ src/Components/Customisation/index.js
@@ -0,0 +1,13 @@
+import CustomBase from './CustomBase';
+import CustomSauce from './CustomSauce';
+import CustomTopping from './CustomTopping';
+import CustomCheese from './CustomCheese';
+import CustomRemarks from './CustomRemarks';
+
+export {
+    CustomBase,
+    CustomSauce,
+    CustomTopping,
+    CustomCheese,
+    CustomRemarks,
+}
