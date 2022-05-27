ba4e7fa091ce6836659180be5e443d0432684e4c
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-27 17:06:29 +0200

    import correctly

--- src/constants/baseImages.js
+++ src/constants/baseImages.js
@@ -1,7 +1,6 @@
-import PizzaBase from '../assets/Base/PizzaBase.png';
+
 import EmptyBase from '../assets/Base/PizzaBaseEmpty.png';
 
 export default [
-    { src: PizzaBase, name: "base", zIndex: "z1" },
     { src: EmptyBase, name: "emptybase", zIndex: "z1" },
 ]
