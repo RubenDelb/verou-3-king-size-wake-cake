3724c34635e29fba885f4a7e09545bb38c130ea7
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-30 14:35:36 +0200

    Dynamically load all the pizzabases with checkmarks

--- src/Components/Customisation/CustomBase/CustomBase.jsx
+++ src/Components/Customisation/CustomBase/CustomBase.jsx
@@ -2,24 +2,73 @@ import AppWrap from '../../AppWrap'
 import './CustomBase.scss'
 import { motion } from 'framer-motion';
 import baseImages from '../../../constants/baseImages';
+import ImageContainer from '../Layout/ImageContainer';
+import CheckmarksContainer from '../Layout/CheckmarksContainer';
+import { useEffect, useState } from 'react';
 
 const CustomBase = () => {
+    const [bases, setBases] = useState({
+        emptybase: false,
+    });
+
+    useEffect(() => {
+        const data = localStorage.getItem("bases");
+        if (data) {
+        setBases(JSON.parse(data));
+        }
+    }, []);
+
+    const onChange = (event, name) => {
+        let newBases = JSON.parse(JSON.stringify(bases));
+        newBases[name] = event;
+        setBases(newBases);
+        localStorage.setItem("bases", JSON.stringify(newBases));
+    };
+
     return (
-        <div className='customize'>
-            <div className='image-container'>
-                <div className='inner-image-container'>
-                    { baseImages.map((base) => {
-                        return (
-                            <div>
-                                <motion.div transition={{ duration: 1 }} className="base">
-                                    <img src={base.src} alt="Pizza Base" height="100%" width="100%" />
-                                </motion.div> 
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
-        </div>
+        <>
+            <ImageContainer>
+                { baseImages.map((base) => {
+                    return (
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{
+                                y: bases[base.name] ? 0 : -200,
+                                opacity: bases[base.name] ? 1 : 0,
+                            }}
+                            transition={{ duration: 1 }}
+                            className={`ingredients ${base.zIndex} ${base.name}`}
+                            key={base.name}
+                        >
+                            <img src={base.src} alt={base.name} height="100%" width="100%" />
+                    </motion.div>
+
+                    )
+                })}
+            </ImageContainer>
+
+            <CheckmarksContainer>
+                { baseImages.map((base) => {
+                    return (
+                        <>
+                            { base.name === "pizzabase" ? "" : (
+                                <label className="container-checkbox">
+                                    {base.name}
+                                    <input
+                                        type="checkbox"
+                                        checked={bases[base.name]}
+                                        onChange={(event) =>
+                                            onChange(event.currentTarget.checked, base.name)
+                                        }
+                                    />
+                                    <span className="checkmark"></span>
+                                </label>
+                            )}
+                        </>
+                    )
+                })}
+            </CheckmarksContainer>
+        </>
         
     )
 }
