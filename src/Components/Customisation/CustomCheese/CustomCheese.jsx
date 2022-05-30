1eb597b158df903ce9e4e3e29e86d998aeb8a47e
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-30 14:46:27 +0200

    Display selected ingredients and store in localstorage

--- src/Components/Customisation/CustomCheese/CustomCheese.jsx
+++ src/Components/Customisation/CustomCheese/CustomCheese.jsx
@@ -1,9 +1,73 @@
+import { useEffect, useState } from 'react';
+import cheeseImages from '../../../constants/cheeseImages';
 import AppWrap from '../../AppWrap';
+import { motion } from 'framer-motion';
+import CheckmarksContainer from '../Layout/CheckmarksContainer';
+import ImageContainer from '../Layout/ImageContainer';
 import './CustomCheese.scss';
 
 const CustomCheese = () => {
+    const [cheeses, setCheeses] = useState({
+        cheese: false,
+    });
+
+    useEffect(() => {
+        const data = localStorage.getItem("cheeses");
+        if (data) {
+        setCheeses(JSON.parse(data));
+        }
+    }, []);
+
+    const onChange = (event, name) => {
+        let newCheeses = JSON.parse(JSON.stringify(cheeses));
+        newCheeses[name] = event;
+        setCheeses(newCheeses);
+        localStorage.setItem("cheeses", JSON.stringify(newCheeses));
+    };
+
+
     return (
-        <div>CustomCheese</div>
+        <>
+            <ImageContainer>
+                { cheeseImages.map((cheese) => {
+                    return (
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{
+                                y: cheeses[cheese.name] ? 0 : -200,
+                                opacity: cheeses[cheese.name] ? 1 : 0,
+                            }}
+                            transition={{ duration: 1 }}
+                            className={`ingredients ${cheese.zIndex} ${cheese.name}`}
+                            key={cheese.name}
+                        >
+                            <img src={cheese.src} alt={cheese.name} height="100%" width="100%" />
+                        </motion.div>
+                    )
+                })}
+            </ImageContainer>
+
+            <CheckmarksContainer>
+                {cheeseImages.map((cheese) => {
+                    return (
+                        <>
+                            <label className="container-checkbox">
+                                {cheese.name}
+                                <input
+                                    type="checkbox"
+                                    checked={cheeses[cheese.name]}
+                                    onChange={(event) =>
+                                        onChange(event.currentTarget.checked, cheese.name)
+                                    }
+                                />
+                                <span className="checkmark"></span>
+                            </label>
+                            
+                        </>
+                    )
+                })}
+            </CheckmarksContainer>
+        </>
     )
 }
 
--- src/Components/Customisation/CustomSauce/CustomSauce.jsx
+++ src/Components/Customisation/CustomSauce/CustomSauce.jsx
@@ -1,9 +1,84 @@
+import sauceImages from '../../../constants/sauceImages';
 import AppWrap from '../../AppWrap';
+import { motion } from 'framer-motion';
+import CheckmarksContainer from '../Layout/CheckmarksContainer';
+import ImageContainer from '../Layout/ImageContainer';
 import './CustomSauce.scss';
+import { useEffect, useState } from 'react';
 
 const CustomSauce = () => {
+    const [sauces, setSauces] = useState({
+        curry: false,
+        pesto: false,
+        tomato: false,
+        spicy: false
+    });
+
+    const [replicaSauces, setReplicaSauces] = useState({
+        curry: false,
+        pesto: false,
+        tomato: false,
+        spicy: false
+    });
+
+    useEffect(() => {
+        const data = localStorage.getItem("sauces");
+        if (data) {
+        setSauces(JSON.parse(data));
+        }
+    }, []);
+
+    const onChange = (event, name) => {
+        let newSauces = JSON.parse(JSON.stringify(replicaSauces));
+        newSauces[name] = event;
+        setSauces(newSauces);
+        localStorage.setItem("sauces", JSON.stringify(newSauces));
+    };
+
+
     return (
-        <div>CustomSauce</div>
+        <>
+            <ImageContainer>
+                {sauceImages.map((sauce) => {
+                    return (
+                        <>
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                animate={{
+                                    y: sauces[sauce.name] ? 0 : -200,
+                                    opacity: sauces[sauce.name] ? 1 : 0,
+                                }}
+                                transition={{ duration: 1 }}
+                                className={`ingredients ${sauce.zIndex} ${sauce.name}`}
+                                key={sauce.name}
+                            >
+                                <img src={sauce.src} alt={sauce.name} height="100%" width="100%" />
+                            </motion.div>
+                        </>
+                    )
+                })}
+            </ImageContainer>
+            <CheckmarksContainer>
+                {sauceImages.map((sauce) => {
+                    return (
+                        <>
+                            <label className="container-checkbox">
+                                {sauce.name}
+                                <input
+                                    type="radio"
+                                    name="sauce-selection"
+                                    checked={sauces[sauce.name]}
+                                    onChange={ (event) =>
+                                        onChange(event.currentTarget.checked, sauce.name)
+                                    }
+                                />
+                                <span className="checkmark"></span>
+                            </label>
+                        </>
+                    )
+                })}
+            </CheckmarksContainer>
+        </>
     )
 }
 
