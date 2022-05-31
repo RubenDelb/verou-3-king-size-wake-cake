92d7d51f4c87084161f5792112c6ae5160bc7968
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-31 17:34:16 +0200

    Include img's from previous steps so they can be displayed

--- src/constants/ingredientsImages.js
+++ src/constants/ingredientsImages.js
@@ -6,14 +6,24 @@ import Tomato from '../assets/Topping/Tomato.webp';
 import Brocoli from '../assets/Topping/broccolis.webp';
 import Carrot from '../assets/Topping/carrots.webp';
 import Eggplant from '../assets/Topping/eggplants.webp';
+import CurrySauce from '../assets/Sauce/curry.webp';
+import PestoSauce from '../assets/Sauce/pesto.webp';
+import TomatoSauce from '../assets/Sauce/tomato.webp';
+import SpicySauce from '../assets/Sauce/spicy.webp';
+import EmptyBase from '../assets/Base/PizzaBaseEmpty.webp';
 
 export default [
-    { src: Basil, name: "basil", zIndex: "z5"},
-    { src: Mushroom, name: "mushroom", zIndex: "z4"},
-    { src: Olive, name: "olive", zIndex: "z3"},
-    { src: Pineapple, name: "pineapple", zIndex: "z3"},
-    { src: Tomato, name: "tomato", zIndex: "z4"},
-    { src: Brocoli, name: "brocoli", zIndex: "z4"},
-    { src: Carrot, name: "carrot", zIndex: "z4"},
-    { src: Eggplant, name: "eggplant", zIndex: "z4"},
+    { src: Basil, name: "basil", zIndex: "z5", category: "topping" },
+    { src: Mushroom, name: "mushroom", zIndex: "z4", category: "topping" },
+    { src: Olive, name: "olive", zIndex: "z3", category: "topping" },
+    { src: Pineapple, name: "pineapple", zIndex: "z3", category: "topping" },
+    { src: Tomato, name: "tomato", zIndex: "z4", category: "topping" },
+    { src: Brocoli, name: "brocoli", zIndex: "z4", category: "topping" },
+    { src: Carrot, name: "carrot", zIndex: "z4", category: "topping" },
+    { src: Eggplant, name: "eggplant", zIndex: "z4", category: "topping" },
+    { src: CurrySauce, name: "curry", zIndex: "z2", category: "sauce" },
+    { src: PestoSauce, name: "pesto", zIndex: "z2", category: "sauce" },
+    { src: TomatoSauce, name: "tomatoSauce", zIndex: "z2", category: "sauce" },
+    { src: SpicySauce, name: "spicy", zIndex: "z2", category: "sauce" },
+    { src: EmptyBase, name: "emptybase", zIndex: "z1", category: "base" },
 ]
--- src/constants/sauceImages.js
+++ src/constants/sauceImages.js
@@ -1,11 +1,14 @@
 import CurrySauce from '../assets/Sauce/curry.webp';
 import PestoSauce from '../assets/Sauce/pesto.webp';
 import TomatoSauce from '../assets/Sauce/tomato.webp';
-import SpicySauce from '../assets/Sauce/spicy.webp'
+import SpicySauce from '../assets/Sauce/spicy.webp';
+import EmptyBase from '../assets/Base/PizzaBaseEmpty.webp';
+
 
 export default [
-    { src: CurrySauce, name: "curry", zIndex: "z2" },
-    { src: PestoSauce, name: "pesto", zIndex: "z2" },
-    { src: TomatoSauce, name: "tomatoSauce", zIndex: "z2" },
-    { src: SpicySauce, name: "spicy", zIndex: "z2" },
+    { src: CurrySauce, name: "curry", zIndex: "z2", category: "sauce" },
+    { src: PestoSauce, name: "pesto", zIndex: "z2", category: "sauce"  },
+    { src: TomatoSauce, name: "tomatoSauce", zIndex: "z2", category: "sauce"  },
+    { src: SpicySauce, name: "spicy", zIndex: "z2", category: "sauce"  },
+    { src: EmptyBase, name: "emptybase", zIndex: "z1", category: "base"  },
 ]
