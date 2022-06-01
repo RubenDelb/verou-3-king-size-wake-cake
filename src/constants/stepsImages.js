e5b58e37a2131cabce969b2c12c79dbdf72ad47a
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-01 11:08:04 +0200

    Replace stepsimages to constants-folder + add extra info

---
+++ src/constants/stepsImages.js
@@ -0,0 +1,13 @@
+import Base from "../assets/home-img/base.webp";
+import Sauce from "../assets/home-img/sauce.webp";
+import Topping from "../assets/home-img/topping.webp";
+import SayCheese from "../assets/home-img/saycheese.webp";
+import Notes from "../assets/home-img/notes.webp";
+
+export default [
+    {src: Base, name: 'base'},
+    {src: Sauce, name: 'sauce'},
+    {src: Topping, name: 'topping'},
+    {src: SayCheese, name: 'cheese'},
+    {src: Notes, name: 'remarks'},
+]
