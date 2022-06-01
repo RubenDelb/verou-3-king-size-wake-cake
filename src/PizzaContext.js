335eb76897f49a7acba6eb49af37c4c9c27d268a
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-01 16:40:31 +0200

    Set values of selected category-items to false

--- src/PizzaContext.js
+++ src/PizzaContext.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { createContext, useState, useEffect } from 'react';
+import cheeseImages from './constants/cheeseImages';
 
 const PizzaContext = createContext();
 
 export function PizzaProvider({children}){
     const [pizzas, setPizzas] = useState({
-        emptybase: false,
+        Deeppan: false,
+        Authentic: false,
         basil: false,
         cheese: false,
         mushroom: false,
@@ -24,20 +26,29 @@ export function PizzaProvider({children}){
     useEffect(() => {
         const data = localStorage.getItem("pizzas");
         data ?
-            setPizzas(JSON.parse(data))
+            setPizzas(JSON.parse(data)) // set the useState to the same value as the localstorage, if it exists
         :
-            localStorage.setItem("pizzas", JSON.stringify(pizzas))
+            localStorage.setItem("pizzas", JSON.stringify(pizzas)) 
+            // If it doesn't exist in Localstorage, set to default-values
     }, []);
 
     const localStorageSaver = () => {
         localStorage.setItem("pizzas", JSON.stringify(pizzas));
     }
 
-    const toggleIngredients = (e) => {
+    const toggleIngredients = (e, item) => {
         const id = e.target.id;
         const checked = e.target.checked;
-        console.log(e.target.checked);
-        setPizzas(prevValue => ({ ...prevValue, [id]: checked }));
+        cheeseImages.map((image) => {
+            if (id === image.name) {
+                // Set the value of the selected checkmark to the selected value (true or false)
+                setPizzas(prevValue => ({ ...prevValue, [id]: checked }));
+            }
+            else if (image.category === item?.category) {
+                // Set all the other values of the same category that has been selected to false, so only 1 can be chosen.
+                setPizzas(prevValue => ({ ...prevValue, [image.name]: false }));
+            }
+        })
     };
 
     return (
