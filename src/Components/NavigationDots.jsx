be54d1db6993bf72d1d40f5aa34132dfefda795c
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-02 13:42:02 +0200

    Style active navigationdot to be bigger

--- src/Components/NavigationDots.jsx
+++ src/Components/NavigationDots.jsx
@@ -10,9 +10,12 @@ const NavigationDots = ({ active }) => {
           to={ `/custom${item.name}` }
           key={ item.name + index }
           className="navigation-dot" 
-          style={ active === item.name ? { backgroundColor: '#FD6F3B' } : {} } 
+          // style={ active === item.name ? { backgroundColor: '#FD6F3B' } : {} } 
         >
-          <img src={ item.src } alt={ item.name }/>
+          <img src={ item.src } 
+            alt={ item.name }
+            style={ active === item.name ? { width: '50px', height: '50px' } : {} }
+          />
         </Link>
       ))}
     </div>
