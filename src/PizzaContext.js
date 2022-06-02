925398a7c86f4d9c8f9d5e2a790a59dc67e82423
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-02 21:54:49 +0200

    Add multiple if-statements to check information

--- src/PizzaContext.js
+++ src/PizzaContext.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { createContext, useState, useEffect } from 'react';
 import cheeseImages from './constants/cheeseImages';
 
@@ -6,7 +5,7 @@ const PizzaContext = createContext();
 
 export function PizzaProvider({children}){
     const [pizzas, setPizzas] = useState({
-        Deeppan: false,
+        Deeppan: true,
         Authentic: false,
         basil: false,
         cheese: false,
@@ -14,44 +13,56 @@ export function PizzaProvider({children}){
         olive: false,
         pineapple: false,
         tomato: false,
-        brocoli: false,
+        broccoli: false,
         eggplant: false,
+        bacon: false,
+        corn: false,
+        onion: false,
+        scampi: false,
+        pepper: false,
+        pepperoni: false,
+        shrimp: false,
         carrot: false,
         curry: false,
         pesto: false,
-        tomatoSauce: false,
+        tomatoSauce: true,
         spicy: false
     });
 
     useEffect(() => {
         const data = localStorage.getItem("pizzas");
         data ?
-            setPizzas(JSON.parse(data)) // set the useState to the same value as the localstorage, if it exists
+            setPizzas(JSON.parse(data)) // set the useState to the same value as the localstorage object
         :
+            // If it doesn't exist in localstorage, set to default-values
             localStorage.setItem("pizzas", JSON.stringify(pizzas)) 
-            // If it doesn't exist in Localstorage, set to default-values
     }, []);
-
-    const localStorageSaver = () => {
-        localStorage.setItem("pizzas", JSON.stringify(pizzas));
-    }
 
     const toggleIngredients = (e, item) => {
         const id = e.target.id;
         cheeseImages.map((image) => {
+            // Check if the clicked button id is the same as the image
             if (id === image.name) {
-                // Set the value of the selected checkmark to the selected value (true or false)
-                setPizzas(prevValue => ({ ...prevValue, [id]: !pizzas[id] }));
+                // This is to prevent that there would be no dough, or no sauce
+                if (pizzas[id] === false || item?.category === 'topping') {
+                    // Set the value of the selected ingredient to the opposite value (true or false)
+                    setPizzas(prevValue => ({ ...prevValue, [id]: !pizzas[id] }));
+                }
             }
-            else if (image.category === item?.category) {
+            else if (image.category === item?.category && item?.category !== "topping") {
                 // Set all the other values of the same category that has been selected to false, so only 1 can be chosen.
+                // Not for the toppings!
                 setPizzas(prevValue => ({ ...prevValue, [image.name]: false }));
             }
         })
     };
 
+    setTimeout(() => {
+        localStorage.setItem("pizzas", JSON.stringify(pizzas));
+    }, 100);
+
     return (
-        <PizzaContext.Provider value={{pizzas, toggleIngredients, localStorageSaver}}>
+        <PizzaContext.Provider value={{pizzas, toggleIngredients}}>
             {children}
         </PizzaContext.Provider>
     )
