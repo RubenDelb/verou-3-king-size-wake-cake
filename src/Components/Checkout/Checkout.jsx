59d2db9eef08509af151e15c704d4816e3fb24bd
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-03 17:02:06 +0200

    Display all selected ingredients before confirm

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Button from '../Button/Button';
+import PizzaContext from '../../PizzaContext';
 import './Checkout.scss';
 
-const Checkout = () => { 
-  
+const Checkout = () => {   
   // TODO: get ingredients from localstorage
+  const {pizzas, toggleIngredients} = useContext(PizzaContext);
 
   const [success, setSuccess] = useState(false);
   const navigate = useNavigate();
@@ -24,21 +24,33 @@ const Checkout = () => {
                 My Ingredients:
               </h1>
               <ul>
-                {/* { Object.keys(ingredients).map((ingredient) => {
+                { Object.keys(pizzas).map((ingredient) => {
                   return (
-                    ingredients[ingredient] && (
-                      <li>
-                        {ingredient[0].toUpperCase()}
-                        {ingredient.substring(1)}
-                      </li>
-                    )
+                    pizzas[ingredient] && (
+                      <p>{ingredient}</p>
+                    ) 
                   )
-                }) } */}
+                  // return (
+                  //   <div>Hey</div>
+                  //   // ingredients[ingredient] && (
+                  //   //   <li>
+                  //   //     {ingredient[0].toUpperCase()}
+                  //   //     {ingredient.substring(1)}
+                  //   //   </li>
+                  //   // )
+                  // )
+                }) }
               </ul>
             </>
           }
-          <Button btnName='Confirm' onClick={() => setSuccess(true)}/>
-          <Button btnName='Go Back' onClick={() => navigate('/customize')}/>
+          <div className="link-div">
+            <button className='link' onClick={() => setSuccess(true)}>
+              Confirm
+            </button>
+            <button className='link' onClick={() => navigate('/customcheese')}>
+              Go Back
+            </button>
+          </div>
         </div>
       </div>
     </div>
