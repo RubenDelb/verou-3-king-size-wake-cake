2dbb7421033fee61e8ca7865d38ad0dc1346fe95
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-03 16:29:40 +0200

    Remove button-component, was unnecessary

--- src/Components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import './Button.scss';
-
-const Button = ({ btnName, onClick }) => {
-    return (
-        <button className="button" onClick={onClick}>{ btnName }</button>
-    )
-}
-
-export default Button
