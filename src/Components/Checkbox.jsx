118515d2a4dc90bd1180ae24a5fe91fa735df555
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-07 18:42:30 +0200

    Add styling for an active ingredient

--- src/Components/Checkbox.jsx
+++ src/Components/Checkbox.jsx
@@ -1,10 +1,17 @@
+import { useContext } from "react";
+import PizzaContext from "../PizzaContext";
+
 const Checkbox = ({ item, toggleIngredients }) => {
+    const { pizzas } = useContext(PizzaContext);
+
+
     return (
         <button 
             className="container-checkbox" 
             key={item.name}
             id={item.name}
             onClick={ (event) => toggleIngredients(event, item) }
+            style={ pizzas[item.name] ? {backgroundColor: "#E0D8B0"} : {backgroundColor: "var(--white-color)"} }
         >
             <img src={item.src2} alt={item.name} height="auto" width="20%" />
             <p>{item.name}</p>
