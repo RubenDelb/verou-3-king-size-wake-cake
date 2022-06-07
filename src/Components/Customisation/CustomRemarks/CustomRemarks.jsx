3c3876a473d0daca177aa3739652a99d6bc616e9
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-07 18:43:29 +0200

    Add buttons to navigate between components

--- src/Components/Customisation/CustomRemarks/CustomRemarks.jsx
+++ src/Components/Customisation/CustomRemarks/CustomRemarks.jsx
@@ -1,46 +1,58 @@
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import NavigationDots from '../../NavigationDots';
 import './CustomRemarks.scss';
 import { useContext } from 'react';
 import PizzaContext from '../../../PizzaContext';
 import cheeseImages from '../../../constants/cheeseImages';
+import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io'
 import { motion } from 'framer-motion';
 
 const CustomRemarks = ({idName}) => {
-  let navigate = useNavigate();
-  const {pizzas, toggleIngredients} = useContext(PizzaContext);
+    let navigate = useNavigate();
+    const {pizzas} = useContext(PizzaContext);
 
-  return (
-    <>
-    <div className="customize">
-        <div className='image-container'>
-          <div className='inner-image-container'>
-            { cheeseImages.map((cheese) => {
-                      return (
-                          <motion.div
-                              initial={{ opacity: 0 }}
-                              animate={{
-                                  y: pizzas[cheese.name] ? 0 : -200,
-                                  opacity: pizzas[cheese.name] ? 1 : 0,
-                              }}
-                              transition={{ duration: 1 }}
-                              className={`ingredients ${cheese.zIndex} ${cheese.name}`}
-                              key={cheese.name}
-                          >
-                              <img src={cheese.src} alt={cheese.name} height="100%" width="100%" />
-                          </motion.div>
-                      )
-            })}
-          </div>
+    return (
+        <>
+        <div className="customize">
+            <div className='image-container'>
+                <div className='inner-image-container'>
+                    { cheeseImages.map((cheese) => {
+                        return (
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                animate={{
+                                    y: pizzas[cheese.name] ? 0 : -200,
+                                    opacity: pizzas[cheese.name] ? 1 : 0,
+                                }}
+                                transition={{ duration: 1 }}
+                                className={`ingredients ${cheese.zIndex} ${cheese.name}`}
+                                key={cheese.name}
+                            >
+                                <img src={cheese.src} alt={cheese.name} height="100%" width="100%" />
+                            </motion.div>
+                        )
+                    })}
+                </div>
+            </div>
+            <div className='remarks'>
+                <h2>Any remarks?</h2>
+                <textarea name="remarks" id="textArea" cols="45" rows="10"></textarea>
+            </div>
+            
         </div>
-    <div className='remarks'>
-      <h2>Any remarks?</h2>
-      <textarea name="remarks" id="textArea" cols="45" rows="10"></textarea>
-    </div>
-    <NavigationDots active={idName} />
-    </div>
-    </>
-  )
+        <NavigationDots active={idName} />
+            <div className="link-div">
+                <Link to={"/customcheese"} className="link"> 
+                    <IoIosArrowBack className='arrow-back' />
+                    Cheese  
+                </Link>
+                <Link to={"/checkout"} className="link"> 
+                    Checkout 
+                    <IoIosArrowForward className='arrow-next' /> 
+                </Link>
+            </div>
+        </>
+    )
 }
 
 export default CustomRemarks;
