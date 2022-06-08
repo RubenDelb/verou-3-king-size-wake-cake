7ae4af6043988925e13c03fa4d1683ddeae4a1dd
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-08 17:01:42 +0200

    Add price to the checkboxes

--- src/Components/Checkbox.jsx
+++ src/Components/Checkbox.jsx
@@ -8,13 +8,14 @@ const Checkbox = ({ item, toggleIngredients }) => {
     return (
         <button 
             className="container-checkbox" 
-            key={item.name}
+            key={item.src}
             id={item.name}
             onClick={ (event) => toggleIngredients(event, item) }
             style={ pizzas[item.name] ? {backgroundColor: "#E0D8B0"} : {backgroundColor: "var(--white-color)"} }
         >
             <img src={item.src2} alt={item.name} height="auto" width="20%" />
-            <p>{item.name}</p>
+            <p className="item-name">{item.name}</p>
+            <p className="price">${item.price}</p>
         </button>
     )
 }
