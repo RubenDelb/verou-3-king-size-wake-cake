bc746af1b12ac7727bae57cf074d97c83a90e98e
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-08 17:02:01 +0200

    Add animation for the navigation-dots

--- src/Components/NavigationDots.jsx
+++ src/Components/NavigationDots.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion';
 import stepsImages from '../constants/stepsImages'
 
 const NavigationDots = ({ active }) => {
   return (
     <div className='navigation'>
       {stepsImages.map((item, index) => (
-        <Link 
-          to={ `/custom${item.name}` }
-          key={ item.name + index }
-          className="navigation-dot" 
-          // style={ active === item.name ? { backgroundColor: '#FD6F3B' } : {} } 
+        <motion.div
+          initial={{ opacity: 0.2 }}
+          animate={{
+              y: active ? 0 : -200,
+              opacity: active === item.name ? 1 : 0.2,
+          }}
+          transition={{ duration: 1 }}
+          key={item.name}
         >
-          <img src={ item.src } 
-            alt={ item.name }
-            style={ active === item.name ? { width: '50px', height: '50px' } : {} }
-          />
-        </Link>
+          <Link
+            to={ `/custom${item.name}` }
+            className="navigation-dot"
+            // style={ active === item.name ? { backgroundColor: '#FD6F3B' } : {} }
+          >
+            <img src={ item.src }
+              alt={ item.name }
+              // style={ active === item.name ? { width: '50px', height: '50px' } : {} }
+            />
+          </Link>
+        </motion.div>
       ))}
     </div>
   )
