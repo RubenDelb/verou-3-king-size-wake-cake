e6229ca1ae07680645fd4516b508f7d04b9cd632
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-09 10:21:32 +0200

    Small changes in steps-description

--- src/constants/stepsImages.js
+++ src/constants/stepsImages.js
@@ -5,9 +5,9 @@ import SayCheese from "../assets/home-img/saycheese.webp";
 import Notes from "../assets/home-img/notes.png";
 
 export default [
-    { src: Base, name: 'base', description: 'Select your base.', title: 'Step 1' },
+    { src: Base, name: 'base', description: 'Select your dough.', title: 'Step 1' },
     { src: Sauce, name: 'sauce', description: 'Select your sauce.', title: 'Step 2' },
-    { src: Topping, name: 'topping', description: 'Select your topping.', title: 'Step 3' },
-    { src: SayCheese, name: 'cheese', description: 'with or without cheese?', title: 'Step 4' },
+    { src: Topping, name: 'topping', description: 'Select your toppings.', title: 'Step 3' },
+    { src: SayCheese, name: 'cheese', description: 'Pick your cheese.', title: 'Step 4' },
     { src: Notes, name: 'remarks', description: 'Any remarks?', title: 'Step 5' },
 ]
