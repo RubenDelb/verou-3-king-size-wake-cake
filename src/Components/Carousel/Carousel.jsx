1aeba63acf0a8287dcbcfbe264bd78cd8b510555
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-09 10:49:39 +0200

    Temp. removal from old carousel

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,52 +1,52 @@
-import { motion } from "framer-motion";
-import { useRef, useState, useEffect } from "react";
-import stepsImages from "../../constants/stepsImages";
+// import { motion } from "framer-motion";
+// import { useRef, useState, useEffect } from "react";
+// import stepsImages from "../../constants/stepsImages";
 
-import "./Carousel.scss";
+// import "./Carousel.scss";
 
-const Carousel = () => {
+// const Carousel = () => {
 
-    const [width, setWidth] = useState(0);
-    const carousel = useRef();
+//     const [width, setWidth] = useState(0);
+//     const carousel = useRef();
 
-    useEffect(() => {
-        console.log(carousel);
-        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
-    }, [])
+//     useEffect(() => {
+//         console.log(carousel);
+//         setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+//     }, [])
 
-    return (
-        <motion.div 
-            ref={carousel}  
-            className="carousel" 
-        >
-            <motion.div 
-                drag="x"
-                dragConstraints={{ right: 0, left: -width }}
-                dragTransition={{ bounceStiffness: 500, bounceDamping: 20 }}
-                dragElastic={0.2}
-                whileTap={{ cursor: "grabbing"}}
-                onDragEnd={
-                    (event, info) => console.log(info.point.x)
-                }
-                dragMomentum={false}
-                className="inner-carousel"
-            >
-                {stepsImages.map((image) => {
-                    return(
-                        <>
-                            <div className="item" key={image.name}> 
-                                <img src={image.src} alt="" />
-                            </div>
-                            <div className="text">
-                                <h1>{image.title}</h1>
-                                <h6>{ image.description }</h6>
-                            </div>
-                        </>
-                    );
-                })}
-            </motion.div>
-        </motion.div>
-    )
-}
+//     return (
+//         <motion.div 
+//             ref={carousel}  
+//             className="carousel" 
+//         >
+//             <motion.div 
+//                 drag="x"
+//                 dragConstraints={{ right: 0, left: -width }}
+//                 dragTransition={{ bounceStiffness: 500, bounceDamping: 20 }}
+//                 dragElastic={0.2}
+//                 whileTap={{ cursor: "grabbing"}}
+//                 onDragEnd={
+//                     (event, info) => console.log(info.point.x)
+//                 }
+//                 dragMomentum={false}
+//                 className="inner-carousel"
+//             >
+//                 {stepsImages.map((image) => {
+//                     return(
+//                         <>
+//                             <div className="item" key={image.name}> 
+//                                 <img src={image.src} alt="" />
+//                             </div>
+//                             <div className="text">
+//                                 <h1>{image.title}</h1>
+//                                 <h6>{ image.description }</h6>
+//                             </div>
+//                         </>
+//                     );
+//                 })}
+//             </motion.div>
+//         </motion.div>
+//     )
+// }
 
-export default Carousel
+// export default Carousel
