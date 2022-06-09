8164272af40f8d527ab71b0d104d5e5ca4f5d625
Dericioso <dery.lantigua@gmail.com>
2022-06-09 10:59:35 +0200

    Add garlic sauce

--- src/constants/cheeseImages.js
+++ src/constants/cheeseImages.js
@@ -19,6 +19,7 @@ import PestoSauce from '../assets/Sauce/pesto.png';
 import TomatoSauce from '../assets/Sauce/tomato-sauce.png'; 
 import SpicySauce from '../assets/Sauce/spicy.png';
 import BarbequeSauce from '../assets/Sauce/barbeque.png';
+import GarlicSauce from '../assets/Sauce/garlic.png'
 import DeepPan from '../assets/Base/thick-dough.png';
 import Authentic from '../assets/Base/thin-dough.png';
 
@@ -43,6 +44,7 @@ export default [
     { src: BarbequeSauce, name: "barbeque", zIndex: "z2", category: "sauce", price: 0 },
     { src: CurrySauce, name: "curry", zIndex: "z2", category: "sauce", price: 0 },
     { src: PestoSauce, name: "pesto", zIndex: "z2", category: "sauce", price: 0 },
+    { src: GarlicSauce, src2: GarlicSauce, name: "garlic", zIndex: "z2", category: "sauce"  },
     { src: SpicySauce, name: "spicy", zIndex: "z2", category: "sauce", price: 0 },
     { src: DeepPan, name: "Deeppan", zIndex: "z1", category: "base", price: 0 },
     { src: Authentic, name: "Authentic", zIndex: "z1", category: "base", price: 0 },
