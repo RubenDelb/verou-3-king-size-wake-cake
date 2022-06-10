060fb7eae84bb41ebc042d5ecd82bcaa7c192a83
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-10 11:20:59 +0200

    Add price for dough

--- src/constants/cheeseImages.js
+++ src/constants/cheeseImages.js
@@ -49,6 +49,6 @@ export default [
     { src: PestoSauce, name: "pesto", zIndex: "z2", category: "sauce", price: 0 },
     { src: GarlicSauce, src2: GarlicSauce, name: "garlic", zIndex: "z2", category: "sauce", price: 0 },
     { src: SpicySauce, name: "spicy", zIndex: "z2", category: "sauce", price: 0 },
-    { src: DeepPan, name: "Deeppan", zIndex: "z1", category: "base", price: 0 },
-    { src: Authentic, name: "Authentic", zIndex: "z1", category: "base", price: 0 },
+    { src: DeepPan, name: "Deeppan", zIndex: "z1", category: "base", price: 7 },
+    { src: Authentic, name: "Authentic", zIndex: "z1", category: "base", price: 7 },
 ]
